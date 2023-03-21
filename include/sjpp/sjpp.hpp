22a6831aa9c9f48df6bfacc8aa90bc2fb10040c7
raxonpc <tomeczekorlowski1@gmail.com>
2023-03-21 20:14:50 +0100

    Initial commit

---
+++ include/sjpp/sjpp.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace sjpp {
+// enum struct ErrorCode {
+
+// }
+
+// std::vector<std::string> get_entries(std::string_view word);
+} // namespace sjpp
---
+++ src/sjpp.cpp
@@ -0,0 +1,10 @@
+#include "sjpp/sjpp.hpp"
+#include <curl/curl.h>
+
+namespace {
+// std::string download_url(const std::string &url) {}
+} // namespace
+
+namespace sjpp {
+// std::vector<std::string> get_entries(std::string_view word) {}
+} // namespace sjpp
